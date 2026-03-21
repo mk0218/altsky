5f9e29e1221825837e297018b19996522c214809
Kim Minkyoung <minkyoung@kodebox.io>
2026-03-21 15:50:29 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import "normalize.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="app">
+  {@render children()}
+</div>
+
+<style>
+  @import "$lib/assets/css/colors";
+  @import "$lib/assets/css/fonts";
+
+  :global(body) {
+    font-family: "Escoredream";
+    font-weight: 400;
+    background-color: var(--bg);
+    color: var(--text);
+  }
+
+  .app {
+    position: absolute;
+    top: 0;
+    max-width: 920px;
+    width: 100vw;
+    height: 100vh;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    padding: 16px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+  }
+</style>
